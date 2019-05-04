e9a8b5b773557bafe9049fb56316b5173f1f1c0b
Sti Beshev <sti.beshev@gmail.com>
2019-05-04 16:00:40 +0300

    Added a Maven plugin to create runnable jar

--- src/main/java/com/zemaster/main/Zemaster.java
+++ src/main/java/com/zemaster/main/Zemaster.java
@@ -6,24 +6,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Zemaster extends Application
+public class Zemaster extends Application 
 {
+	
 	public static void main(String[] args) 
 	{
 		launch(args);
 	}
-	
+
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
+	public void start(Stage primaryStage) throws Exception 
+	{
 		primaryStage.setTitle("Zemaster");
-		
+
 		Parent mainParent = FXMLLoader.load(getClass().getResource("/Main.fxml"));
-		
+
 		Scene scene = new Scene(mainParent, 800, 600);
-		
+
 		primaryStage.setScene(scene);
-		
+
 		primaryStage.show();
 	}
 
