ce403dc7dec8074b774265611c12566a5be2d309
S. Beshev <sti.beshev@gmail.com>
2019-06-04 01:00:18 +0300

    Added logging for showing the about dialog

--- src/main/java/com/zemaster/controllers/HelpController.java
+++ src/main/java/com/zemaster/controllers/HelpController.java
@@ -1,9 +1,13 @@
 package com.zemaster.controllers;
 
+import com.zemaster.logging.ZeLogMaster;
+import com.zemaster.logging.ZemasterLogger;
+
 import javafx.scene.control.Alert;
 
 public class HelpController
 {
+	private ZemasterLogger logger = ZeLogMaster.getZeLogger();
 	private static HelpController helpController;
 	
 	private HelpController()
@@ -14,8 +18,10 @@ public class HelpController
 	{
 		Alert aboutAlert = new Alert(Alert.AlertType.INFORMATION);
 		aboutAlert.setHeaderText("Zemaster");
-		aboutAlert.setContentText("version: 0.0.1");
+		aboutAlert.setContentText("version: 0.1.0");
 		aboutAlert.show();
+		
+		logger.logInfo("Showing the about dialog");
 	}
 	
 	public static HelpController getInstances()
